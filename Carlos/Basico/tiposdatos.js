0bd1ad40fa63dd5e92d4cfd393b1ce7d5787ef6f
pazthor.ras.root.17@gmail.com <carlos.paz@patagonia-it.com>
2020-07-09 12:06:40 -0400

    lo basico tipo de datos

---
+++ Carlos/Basico/tiposdatos.js
@@ -0,0 +1,46 @@
+// tipos de datos
+
+/*
+############
+Boolean
+= true | false
+null
+= null
+###########
+undefined
+= undefined
+############
+Number
+= float
+-(2^53 - 1)
++(2^53 - 1)
+NaN
++Infinity
+-Infinity
+##############
+String
+= template string
+const mundo = "hola mundo"
+console.log(`${mundo} este es mundo`)
+
+################
+Symbol
+################
+Object
+= en mi pueblo es diccionario
+#################
+Array
+= en mi pueblo es listas
+
+######################
+operadores
+suma =  +, +=
+resta = -, -=
+multiplicar = *
+
+
+
+
+*/
+
+console.log(typeof(undefined))
