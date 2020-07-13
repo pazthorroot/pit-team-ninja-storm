899e0b23abbfa444c42eec7973123a6f1077b3a9
pazthor.ras.root.17@gmail.com <carlos.paz@patagonia-it.com>
2020-07-13 12:36:23 -0400

    sentecias

---
+++ Carlos/Basico/promesas.js
@@ -0,0 +1,39 @@
+// sentencias
+/*
+if (condition) {
+
+}else if (condition) {
+
+}
+
+switch (key) {
+  case value:
+
+    break;
+
+  default:
+    break;
+}
+
+try {
+
+} catch (error) {
+
+}
+
+throw lanzar una excepción
+
+*/
+// condiciones && = and  || = or
+let carlos = ""
+if (carlos) {
+  console.log("verdadero")
+}else if (condition) {
+
+}
+// operaciones ternarias
+// condición ? expresión1 : expresión2;
+// variable = expresión ? true_value : false_value;
+// var max = ( num1 > num2 ) ? num1 : num2 ;
+// Porque javascript es asíncrono?
+// https://lemoncode.net/lemoncode-blog/2018/1/29/javascript-asincrono
