b0637f60ddb8baf57765e0b945b79ba94193c236
jvillane <jvillane@gmail.com>
2020-07-27 21:27:13 -0400

    REFACTOR: algorithms been moved to sub folder ADD: data structure new folder and initial content

---
+++ data_structures_algorithms/data_structures/tree.ts
@@ -0,0 +1,33 @@
+export interface Node {
+  value: number
+  left?: Node
+  right?: Node
+}
+
+export class Tree {
+  root?: Node;
+
+  add(value: number) {
+    if (this.root) {
+      this.addNode(this.root, value);
+    } else {
+      this.root = {value};
+    }
+  }
+
+  addNode(node: Node, value: number) {
+    if (value <= node.value) {
+      if (node.left) {
+        this.addNode(node.left, value);
+      } else {
+        node.left = {value};
+      }
+    } else {
+      if(node.right) {
+        this.addNode(node.right, value);
+      } else {
+        node.right = {value};
+      }
+    }
+  }
+}
