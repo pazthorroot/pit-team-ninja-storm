43c3d355cfffb87681ed1d3ed0ed7d1d536034c9
pazthor.ras.root.17@gmail.com <carlos.paz@patagonia-it.com>
2020-08-07 12:27:39 -0400

    feact: models en django

--- Carlos/Python/django/curso1/almacen/admin.py
+++ Carlos/Python/django/curso1/almacen/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
-
+from .models import Marca, Producto
 # Register your models here.
+admin.site.register(Marca)
+admin.site.register(Producto)
+
+#####################
+'''
+crear un registro
+
+>>> from almacen.models import Marca, Producto
+>>> marca = Marca(nombre_mar='Diwod')
+>>> marca.save()
+>>> query = Marca.object.get(nombre_mar='Diwod')
+>>> query.id_mar
+4
+>>> all = Producto.objects.all()
+>>> all[0].marca_pro
+>>> all[0].marca_pro.nombre_mar
+
+'''
--- Carlos/Python/django/curso1/almacen/models.py
+++ Carlos/Python/django/curso1/almacen/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Marca(models.Model):
+  id_mar = models.AutoField(primary_key=True)
+  nombre_mar = models.CharField(max_length=50)
+
+  def __str__(self):
+    return self.nombre_mar
+
+class Producto(models.Model):
+  id_pro = models.AutoField(primary_key=True)
+  marca_pro = models.ForeignKey(Marca, on_delete=models.CASCADE)
+  nombre_pro = models.CharField(max_length=30)
+  cantidad_pro = models.IntegerField()
+  precio_pro = models.FloatField()
+  existencia_pro = models.BooleanField()
+  def __str__(self):
+    return self.nombre_pro
